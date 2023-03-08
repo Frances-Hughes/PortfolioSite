be45de2c88b0342759ca4df4044a53e66c9d973a
Frances Hughes <112314105+Frances-Hughes@users.noreply.github.com>
2023-03-08 23:53:33 +1300

    added testimonials page

--- portfolio_starter-main/src/components/About.jsx
+++ portfolio_starter-main/src/components/About.jsx
@@ -3,7 +3,94 @@ import React from 'react';
 import Testimonial from './Testimonial';
 
 const About = () => {
-  return <></>;
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-round',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '2rem',
+    },
+  }));
+
+  const CustomtitleBox = styled(Box)(({ theme }) => ({
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '2rem',
+    },
+  }));
+
+  const CustomButton = styled(Button)(({ theme }) => ({
+    border: '3px solid white',
+    borderRadius: '25px',
+    color: 'white',
+    width: '15%',
+    display: 'block',
+    '&hover': {
+      border: '3px solid #00C7FF',
+    },
+    [theme.breakpoints.down('md')]: {
+      width: '50%',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '75%',
+    },
+  }));
+
+  return (
+    <>
+      <Box
+        sx={{ maxWidth: '1300px', my: 10, padding: 3, mx: 'auto' }}
+        id="about"
+      >
+        <CustomtitleBox>
+          <Typography variant="h4" sx={{ color: 'white', textAlign: 'center' }}>
+            What people say about my work!
+          </Typography>
+          <div
+            style={{
+              height: '5px',
+              backgroundColor: '#00C7FF',
+              width: '50%',
+              border: '0',
+              borderRadius: '25px',
+            }}
+          ></div>
+        </CustomtitleBox>
+
+        <CustomBox sx={{ my: 7 }}>
+          <Testimonial />
+          <Testimonial />
+          <Testimonial />
+        </CustomBox>
+
+        <div
+          style={{
+            height: '5px',
+            backgroundColor: '#00C7FF',
+            width: '100%',
+            border: '0',
+            borderRadius: '25px',
+          }}
+        ></div>
+
+        <Typography
+          variant="h4"
+          sx={{ color: 'white', mt: 12, textAlign: 'center' }}
+        >
+          Interested in Working Together?
+        </Typography>
+
+        <CustomButton variant="outlined" sx={{ mx: 'auto', mt: 3, mb: 8 }}>
+          Get in Touch
+        </CustomButton>
+      </Box>
+    </>
+  );
 };
 
 export default About;
--- portfolio_starter-main/src/components/Testimonial.jsx
+++ portfolio_starter-main/src/components/Testimonial.jsx
@@ -1,12 +1,49 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
-import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
-
-import React from "react";
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 
 const Testimonial = () => {
-  return <></>;
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: '#000F2E',
+          padding: 4,
+          border: '1px solid #00C7FF',
+          borderRadius: '10px',
+          maxWidth: '315px',
+          position: 'relative',
+          mx: 3,
+        }}
+      >
+        <Typography variant="body1" sx={{ color: 'white', mb: 2 }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+          eligendi hic tempora consectetur odio placeat dignissimos corrupti
+          distinctio eaque quaerat, ipsa iste quod dolore minima blanditiis a?
+          Distinctio, sapiente similique.
+        </Typography>
+
+        <Typography variant="body2">
+          <span style={{ color: '#33C7FF', fontWeight: 'bold' }}>
+            Millie Hughes
+          </span>
+          <span style={{ color: '#7B89AB' }}>Student of DevAcademy</span>
+        </Typography>
+
+        <FormatQuoteIcon
+          style={{
+            position: 'absolute',
+            top: '-20px',
+            left: '-20px',
+            color: '#00C7FF',
+            height: '50px',
+            width: '50px',
+          }}
+        ></FormatQuoteIcon>
+      </Box>
+    </>
+  );
 };
 
 export default Testimonial;
