48b5ea54f7a4d009da116c01243f7cad2ec6a206
Frances Hughes <112314105+Frances-Hughes@users.noreply.github.com>
2023-03-08 19:37:16 +1300

    started

---
+++ portfolio_starter-main/src/components/Project.jsx
@@ -0,0 +1,28 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
+import Chip from '@mui/material/Chip';
+import { Box } from '@mui/system';
+import React from 'react';
+
+const Project = () => {
+
+  return <></>
+//   return <Card sx={{maxWidth: 375, margin: "1rem"}}>
+//     <CardActionArea>
+//       <CardMedia component="img" image={img} />
+//       <CardContent>
+//         <Typography variant="h5" component="div">
+          
+//         </Typography>
+//       </CardContent>
+//     </CardActionArea>
+// }
+
+export default Project;
